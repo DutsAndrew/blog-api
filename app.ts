84ec3a7b22c89f4485fa3f488cd3013d9a4cf225
Andrew Dutson <Dutsandrew@gmail.com>
2023-03-28 21:16:21 -0600

    encoded set to true, try/catch for signup moved inside hash function

--- app.ts
+++ app.ts
@@ -45,7 +45,7 @@ app.use(compression());
 app.use(limiter);
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/', appRouter);
 app.use('/api', apiRouter);
